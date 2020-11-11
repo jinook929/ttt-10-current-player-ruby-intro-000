7c2c4151f74b514ef1700ba3a3aba3fd54d512fb
JINOOK JUNG <jinook929@gmail.com>
2020-11-11 02:54:33 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,7 @@ def turn_count(board)
   counter = 0
   index = 0
   while(index < 9)
-    if(board[index] != " " || board[index] != "" || board[index] != nil)
+    if(!(board[index] == " " || board[index] == "" || board[index] == nil))
       counter += 1
     end
     index += 1
